86f31a74d6feb777a963ee7bd3e6937f41b430ee
steven choi <steven.choi@csiro.au>
2023-09-14 15:36:37 +1000

    #777 remove Listcontroller and opus/list

--- grails-app/controllers/au/org/ala/profile/UrlMappings.groovy
+++ grails-app/controllers/au/org/ala/profile/UrlMappings.groovy
@@ -6,7 +6,6 @@ class UrlMappings {
 
         // Public API
         "/api/v1/opus/$opusId/export" controller: "export", action: [GET: "exportCollection"]
-        "/opus/list" controller: "list", action: [GET: "listCollections"]
         "/api/v1/opus/$opusId/count" controller: "export", action: [GET: "countProfiles"]
 
         "/api/v1/profiles/" controller: "export", action: [GET: "getProfiles"]
