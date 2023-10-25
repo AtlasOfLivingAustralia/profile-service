36c9823953c64f89e12f90f9a581f31cd47ffa63
steven choi <steven.choi@csiro.au>
2023-10-25 11:42:57 +1100

    #798 remove function

--- src/scripts/mongo/exportImageData.js
+++ src/scripts/mongo/exportImageData.js
@@ -3,13 +3,13 @@ var profiles = db.getCollection('profile').find({'privateImages':{$exists: true}
 var privateImages = ['Profile id,Image id,Title,Description,Rights Holder,Rights,Licence,Creator,Original File Name,Created']
 
 const escapeComma = function(data) {
-    if (data.toString().includes(',')) {
+    if (data.includes(',')) {
         return data.replace(/,/g,"");
     }
     return data;
 }
-profiles.forEach(p => p.privateImages.forEach(i => privateImages.push(escapeComma(p._id) + "," + escapeComma(i.imageId) + "," + escapeComma(i.title) + ","
+profiles.forEach(p => p.privateImages.forEach(i => privateImages.push(p._id + "," + i.imageId + "," + escapeComma(i.title) + ","
     + escapeComma(i.description) + "," + escapeComma(i.rightsHolder) + "," + escapeComma(i.rights) + "," + escapeComma(i.licence) + "," + escapeComma(i.creator)
-    + "," + escapeComma(i.originalFileName) + "," + escapeComma(i.created))));
+    + "," + escapeComma(i.originalFileName) + "," + i.created)));
 
 privateImages.forEach(i => print(i));
