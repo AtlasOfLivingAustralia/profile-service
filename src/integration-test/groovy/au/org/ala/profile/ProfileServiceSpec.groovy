0efc662dd26299e35b5a0bff01d9862b1af52b57
sal153 <joseph.salomon@csiro.au>
2026-09-02 07:54:22 +1000

    debugging test issue #922

--- src/integration-test/groovy/au/org/ala/profile/ProfileServiceSpec.groovy
+++ src/integration-test/groovy/au/org/ala/profile/ProfileServiceSpec.groovy
@@ -1970,7 +1970,7 @@ class ProfileServiceSpec extends BaseIntegrationSpec {
         Map created = service.createDocument(profile, [name: "Original title", type: "audio", url: "https://soundcloud.com/example/original"])
 
         then:
-        assert created.status == 'ok' : created.error
+        assert created.status == 'ok' : "createDocument failed: ${created.error}"
         created.documentId
         profile.documents.size() == 1
         profile.documents[0].name == "Original title"
